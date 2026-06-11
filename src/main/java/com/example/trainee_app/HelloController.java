af6cd14b96a6a0843daa46c04e3809fe8a22987c
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-06-11 09:40:05 +0400

    Task 4: Creating Your First Controller (The Waiter)

--- src/main/java/com/example/trainee_app/HelloController.java
+++ src/main/java/com/example/trainee_app/HelloController.java
@@ -1,6 +1,8 @@
 package com.example.trainee_app;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class HelloController {
     @GetMapping("/hello")
