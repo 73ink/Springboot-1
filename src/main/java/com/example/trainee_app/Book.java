9d2179e0aae7566462814f12b2188ad95a0b1ea2
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-06-14 10:19:16 +0400

    created Book.java and  declare two private variables (integer, string)

---
+++ src/main/java/com/example/trainee_app/Book.java
@@ -0,0 +1,14 @@
+package com.example.trainee_app;
+
+public class Book {
+
+    private int id;
+    private String name;
+
+    public Book(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+
+}
