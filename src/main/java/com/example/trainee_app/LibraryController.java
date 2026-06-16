0df15daf2ad737f6003d8298c3e1c838ee19189b
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-06-16 09:54:31 +0400

    Task 19: The Master Ecosystem Controller (Managing Two Bookshelves) [completed]

--- src/main/java/com/example/trainee_app/LibraryController.java
+++ src/main/java/com/example/trainee_app/LibraryController.java
@@ -1,4 +1,34 @@
 package com.example.trainee_app;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
 public class LibraryController {
+
+    private static List<Author> authorList = new ArrayList<>();
+    private static List<Book> bookList = new ArrayList<>();
+
+    @GetMapping("/add-author")
+    public String addAuthor(@RequestParam int id,
+                            @RequestParam String name,
+                            @RequestParam String biography) {
+
+        Author newAuthor = new Author(id, name, biography);
+
+        authorList.add(newAuthor);
+
+        return "Author added successfully!";
+    }
+
+    @GetMapping("/all-authors")
+    public List<Author> getAllAuthors() {
+
+        return authorList;
+    }
+
 }
