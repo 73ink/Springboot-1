9f2b33fa3723699613d5aa12be9ac518c7cdc2d4
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-06-17 10:17:40 +0400

    Task 22: Inventory Book Data Structure, added private variables store the book inventory details

--- src/main/java/com/example/trainee_app/InventoryBook.java
+++ src/main/java/com/example/trainee_app/InventoryBook.java
@@ -1,4 +1,12 @@
 package com.example.trainee_app;
 
 public class InventoryBook {
+
+    // Task 22: Inventory Book Data Structure
+    // These private variables store the book inventory details.
+    private int bookId;
+    private String title;
+    private double price;
+    private int stockCount;
+
 }
